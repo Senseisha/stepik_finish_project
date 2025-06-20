057e0b273939febd1b752cb9010c86f7a7783f24
Anastasia Troitskaya <senseisha@yandex.ru>
2025-06-20 13:42:45 +0300

    Наследование и отрицательные проверки на отсутствие товаров в корзине

--- pages/base_page.py
+++ pages/base_page.py
@@ -65,3 +65,13 @@ class BasePage():
 
     def should_be_login_link(self):
         assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+
+    def go_to_basket(self):
+        self.browser.find_element(*BasePageLocators.BASKET_BUTTON).click()
+
+    def should_not_be_product_in_basket(self):
+        assert self.is_not_element_present(*BasePageLocators.NOT_PRODUCT), "Product in basket, but should not be"
+
+    def should_be_message_basket_is_empty(self):
+        message = self.browser.find_element(*BasePageLocators.BASKET_IS_EMPTY).text
+        assert message.startswith("Ваша корзина пуста")
--- test_product_page.py
+++ test_product_page.py
@@ -48,9 +48,17 @@ import time
 #     page.open()
 #     page.should_be_login_link()
 
-def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
+# def test_guest_can_go_to_login_page_from_product_page(browser):
+#     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+#     page = ProductPage(browser, link)
+#     page.open()
+#     page.go_to_login_page()
+#     assert browser.current_url == "https://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
     page.open()
-    page.go_to_login_page()
-    assert browser.current_url == "https://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page.go_to_basket()
+    page.should_not_be_product_in_basket()
+    page.should_be_message_basket_is_empty()
